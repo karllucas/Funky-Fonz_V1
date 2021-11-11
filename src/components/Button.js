a30e43d08e78f869a4d7256631d900ddd99fc7ca
karllucas <karlkamakia@gmail.com>
2021-11-11 15:03:06 +0300

    Details Page Button Commit Three

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ text-transform:capitalize;
 font-size:1.4rem;
 background : ${ props => props.cart ? "var(--mainGreen)" : "var(--mainBlue)"};
 border:1px solid;
-border-color: ${ props => props.cart ? "var(--mainGreen)" : "var(--mainGreen)"};
+border-color: ${ props => props.cart ? "var(--mainGreen)" : "var(--mainBlue)"};
 border-radius:25px;
 padding:0.2rem 1rem;
 cursor:pointer;
@@ -14,9 +14,9 @@ margin:0 auto;
 transition: all 0.5s ease-in-out;
 &:hover {
     border:1px solid;
-    border-color: ${ props => props.cart ? "var(--mainGreen)" : "var(--secDark)"};
-    background:${ props => props.cart ? "var(--mainBlue)" : "var(--mainDark)"};
-    color:${ props => props.cart ? "var(--mainGreen)" : "var(--mainWhite)"};
+    border-color: ${ props => props.cart ? "var(--mainDark)" : "var(--mainOrange)"};
+    background:${ props => props.cart ? "var(--mainDark)" : "var(--mainOrange)"};
+    color:${ props => props.cart ? "var(--mainWhite)" : "var(--mainDark)"};
 }
 &:focus {
     outline:none;
