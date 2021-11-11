0d21313ff828af3427e1c35fa716c962fd212b92
karllucas <karlkamakia@gmail.com>
2021-11-12 01:07:33 +0300

    Sticky Footer Commit Two

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Footer extends Component {
     render() {
         return (
-            <footer className="container-fluid fixed-bottom">
+            <footer className="container-fluid">
                 <div className="row bg-light">
                     <div className="mx-auto text-center py-5 text-bright-blue">&copy;Funky Phones All Rights Reserved</div>
                 </div>
