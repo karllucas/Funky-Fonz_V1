23c26a8431c1b6e0a3569ce67f00b9d9b303b427
karllucas <karlkamakia@gmail.com>
2021-12-08 17:16:38 +0300

    Footer changes one

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function EmptyCart() {
     return (
-      <div className="container mt-5">
+      <div className="container mt-5 min-vh-100">
           <div className="row m-0">
               <div className="col-10 m-0 mx-auto text-center text-title pt-5">
                   <h1 className="larger-view m-0 pt-5">your cart is currently empty</h1>
